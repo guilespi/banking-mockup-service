2436721777f6692035119f72a3cfc370e8bff1fc
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-03-08 19:57:25 -0200

    Nueva API que responde datos de la posicion consolidada en /products

--- src/banking_service/server.clj
+++ src/banking_service/server.clj
@@ -37,12 +37,43 @@
    {:id 2 :description "Pasaporte"}
    {:id 3 :description "DNI"}])
 
+(defn- get-user-products
+  []
+  [{:product-type "tarjeta"
+    :product-name "Tarjeta de Credito VISA Internacional"
+    :product-category "Tarjeta VISA"
+    :card-number "5443 1231 2222 0226"
+    :usd-debt "U$S 1.520,00"
+    :uyp-debt "$ 8.930.560,20"}
+   {:product-type "prestamo"
+    :product-name "Prestamo Hipotecario 15 anios Tasa Fija"
+    :product-category "Prestamos Vivienda"
+    :loan-id "91812122521"
+    :loan-ammount "$ 2.320.000,00"
+    :paid-ammount "$ 16.000,00"
+    :remaining-ammount "36/180"
+    :next-due-date "VTO. 01/11/2011"}
+   {:product-type "cuenta-corriente"
+    :product-name "Cuenta Corriente Pesos Uruguayos"
+    :product-category "Cuenta Cheques"
+    :account-number "123123125071"
+    :account-balance "$ 142.320.226,00"}
+   {:product-type "plazo-fijo"
+    :product-name "Plazo Fijo"
+    :product-category "Ahorros para Universidad Pablo"
+    :plazo-id "234234237560"
+    :plazo-ammount "U$S 26.000,00"
+    :due-date "Vencimiento 31/08/2013"
+    :plazo-tasa "TASA 2,5%"}])
+
 (defroutes main-routes
   (GET "/definitions" [] (app-files "resources/public"))
   (GET "/newsfeed" [] (-> (response/response (json/write-str (newsfeed)))
                           (response/content-type "application/json")))
   (GET "/login/documents" [] (-> (response/response (json/write-str (document-types)))
                                  (response/content-type "application/json")))
+  (GET "/products" [] (-> (response/response (json/write-str (get-user-products)))
+                          (response/content-type "application/json")))
   (route/resources "/")
   (route/not-found (do (println "Unknown url received")
                        "Page not found")))
